0418cbbbd7e725594af39431e0409f850a381497
Dmitri Stepanenko <79631529+DWH0AMIR@users.noreply.github.com>
2022-01-21 11:05:13 +0300

    Initial commit. Car model was added and enum as well.

---
+++ Automarket.Domain/Enums/TypeCar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automarket.Domain.Enums;
+
+public enum TypeCar
+{
+    [Display(Name = "Легковой автомобиль")]
+    PassengerCar = 0,
+
+    [Display(Name = "Седан")]
+    Sedan,
+
+    [Display(Name = "Хэтчбек")]
+    HatchBack,
+
+    [Display(Name = "Минивэн")]
+    Minivan,
+
+    [Display(Name = "Спортивная машина")]
+    SportsCar,
+
+    [Display(Name = "Внедорожник")]
+    Suv
+}
---
+++ Automarket.Domain/Models/Car.cs
@@ -0,0 +1,27 @@
+﻿using Automarket.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automarket.Domain.Models;
+
+public class Car
+{
+    public int Id { get; set; } = default!;
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string Model { get; set; }
+    
+    public float Speed { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateTime DateOfCreation { get; set; }
+
+    public TypeCar TypeOfCar { get; set; }
+}
